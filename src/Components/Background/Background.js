04edf3c579cae59763a49a87a5f449f96edff46b
haneeenedinat <haneen.aledinat@gmail.com>
2021-12-30 23:36:19 +0200

    background

--- src/Components/Background/Background.js
+++ src/Components/Background/Background.js
@@ -3,9 +3,24 @@ import './Background.css'
 
 export default function Background(){
     return(
-        <div className="background"
-        style={{background: " url('../img/bg.jpg')",backgroundRepeat:'no-repeat',backgroundSize:'cover', backgroundPosition: 'center'  }}
+        
+        <div className="background">
+
+        <div className="backgroundImg"
+        style={{
+            background: `linear-gradient(190deg, #ffffff , rgba(0, 0, 1, 0)), url('../img/bg.jpg')`,
+            backgroundRepeat: "no-repeat",
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+          }}
         >
+
+            <div className="TextBackground">
+              <div> we are...</div>
+              <div>an event, design, planning and creative consulting company specializing in customized experiences for our clients and their guests.</div>
+            </div>
+
+        </div>
 
         </div>
     )
