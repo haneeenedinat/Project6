8a2c8fe2bb7d87eaf321716316cc78ab60320a35
haneeenedinat <haneen.aledinat@gmail.com>
2022-01-03 02:35:14 +0200

    checkout2

--- src/Components/CheckOut/CheckOut.js
+++ src/Components/CheckOut/CheckOut.js
@@ -1,91 +1,137 @@
 import React, { useState } from "react";
+import {  useNavigate } from "react-router-dom"
+import "./CheckOut.css";
 
-export default function CheckOut() {
-  const SelectedServices = JSON.parse(localStorage.getItem("SelectedServices"));
-
-
-  const [SelectedServicesObj, setSelectedServicesObj] = useState({
-    title: SelectedServices.title,
-    price: SelectedServices.price,
-  });
-  const [DiscountInput,setDiscountInput]=useState();
-  const [Total,setTotal]=useState();
-
-  const handleDiscount=(e)=>{
-    setDiscountInput(e.target.value);
-  
-  }
-
-  const handleSubmite=()=>{
-      console.log(DiscountInput)
-      if(DiscountInput=='haneen'){
-     
-
+export default function NewPath() {
+  const UserLogin=JSON.parse(localStorage.getItem('UserLogin'))
+  const navigate = useNavigate();
+  const handelNavigate = () => {
+    if(UserLogin){
+        navigate("/");
+        console.log('hhhhh')
     }
-         // {Math.round(Number(SelectedServicesObj.price * 0.04 + total()) * 100) / 100}
-         let priceAfter=Number(SelectedServices.price)
-         let DiscountPercentage=priceAfter*0.2;
-         console.log(DiscountPercentage)
-      console.log(priceAfter)
+    else{
+        navigate('/LoginPage')
+    }
+  };
+  return <CheckOut handelNavigate={handelNavigate} />;
+}
+
+function CheckOut(props) {
+  const SelectedServices = JSON.parse(localStorage.getItem("SelectedServices"));
+ 
+
+  const [DiscountInput, setDiscountInput] = useState();
+  const [Total, setTotal] = useState();
+  const [number, setNumber] = useState();
+  const [MessageNumber, setMessageNumber] = useState();
+
+  const handleDiscount = (e) => {
+    setDiscountInput(e.target.value);
+  };
+
+  const handleSubmite = (e) => {
+    e.preventDefault();
+    console.log(DiscountInput);
+    if (DiscountInput === "haneen") {
+      let priceBefore = Number(SelectedServices.price);
+      let DiscountPercentage = priceBefore * 0.2;
+      console.log(DiscountPercentage);
+      console.log(priceBefore);
+      let priceAfter = priceBefore - DiscountPercentage;
+      console.log(priceAfter);
+      setTotal(priceAfter);
+    }
+    console.log(e.target);
+    e.target.reset();
+  };
+
+  const handleSubmiteform = (e) => {
+    e.preventDefault();
+    if (number.length !== 10) {
+      setMessageNumber("The number consists of 10 digits");
+    } else {
+      setMessageNumber("");
+      props.handelNavigate();
+    }
+
+
+    
   }
 
   return (
     <div className="CheckOut">
-      <div className="CheckOutTitle">
-        <h2>CheckOut Page</h2>
-      </div>
+      <div className="CheckOutTitle">{/* <h2>CheckOut Page</h2> */}</div>
 
-      <div>
-         <h2>Discount 20%</h2>
+      <div className="discountDetails">
+        <h2>Discount {"  "} 20%</h2>
         <h2>Discount code : haneen</h2>
-     
       </div>
 
-      <div className="CheckOutForm">
-        <form>
-          <div className="PersonalInformationCheckOut">
-            <p>Personal Information</p>
-            <input type="text" placeholder="UserName" required />
-            <input type="email" placeholder="Email" required />
-            <input type="tel" placeholder="Mobile Number" required />
+      <div className="CheckOutFormANDDiscountSection">
+        <div className="CheckOutForm">
+          <div className="CheckOutFormTitle">
+            <h4>CheckOut Form </h4>
           </div>
 
-          <div className="AddressInformationCheckOut">
-            <p>Address Information</p>
-            <input type="text" placeholder="Address1" required />
-            <input type="text" placeholder="Address2" />
+          <form onSubmit={handleSubmiteform}>
+            <div className="PersonalInformationCheckOut">
+              <p>Personal Information</p>
+              <input type="text" placeholder="UserName" className="input" required />
+              <input type="email" placeholder="Email" className="input"  required />
+              <input
+                type="text"
+                placeholder="Mobile Number"
+                className="input"
+                required
+                onChange={(e) => {
+                  setNumber(e.target.value);
+                }}
+              />
+              <small>{MessageNumber}</small>
+            </div>
+
+            <div className="AddressInformationCheckOut">
+              <p>Address Information</p>
+              <input type="text" placeholder="Address1" className="input" required />
+              <input type="text" placeholder="Address2" className="input"/>
+            </div>
+
+            <div>
+              <input type="checkbox"  required /> <span>Cash on delivery</span>
+             
+            </div>
+            <div>
+              <button>Checkout</button>
+            </div>
+          </form>
+        </div>
+
+        <div className="CheckOutFormDiscount">
+          <div className="CheckOutFormDiscountTitle">
+            <h4>Your Order</h4>
           </div>
 
-          <div>
-            <input type="checkbox" required />
-            Cash on delivery
+          <div className="SelectedServicesName">
+            <p>Services Name :{SelectedServices.title} </p>
           </div>
-        </form>
-      </div>
 
-      <div className="CheckOutFormDiscount">
-        <div className="CheckOutFormDiscountTitle">
-          <h4>Your Order</h4>
-        </div>
+          <div className="SelectedServicesPrice">
+            <p>Services Price :{SelectedServices.price}</p>
+          </div>
 
-        <div className="SelectedServicesName">
-          <p>Services Name :{SelectedServices.title} </p>
-        </div>
-
-        <div className="SelectedServicesPrice">
-          <p>Services Price :{SelectedServices.price}</p>
-        </div>
-
-        <div className="SelectedServicesTotal">
-          <p></p>
-        </div>
-
-        <div className="DiscountInput">
-          <h4>Discount Code</h4>
-          <input type="text" placeholder="Discount Code" onChange={(e)=>{handleDiscount(e)}} />
-          <button type="submit" onClick={handleSubmite}>Discount</button>
-          <p>The Price After Discount :{Total} </p>
-          
+          <div className="DiscountInputSection">
+            <h3>Discount Code</h3>
+            <form className="DiscountInputForm" onClick={handleSubmite}>
+              <input
+                type="text"
+                placeholder="Discount Code"
+                onChange={handleDiscount}
+              />
+              <button type="submit">Discount</button>
+            </form>
+            <p> <strong>The Price After Discount :{Total} </strong></p>
+          </div>
         </div>
       </div>
     </div>
